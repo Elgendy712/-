a0de6248e7d980e0dfdba822b5e4e531418ef9e3
Elgendy712 <168934321+Elgendy712@users.noreply.github.com>
2024-05-04 23:58:54 +0300

    Add files via upload

--- import cv2.py
+++ import cv2.py
@@ -25,17 +25,13 @@ def split_and_compute_histograms(image):
     hist_r = compute_histogram(r)
     return b, g, r, hist_b, hist_g, hist_r
 
-# Load image
 image_path ="D:\course\WhatsApp Image 2024-05-04 at 23.28.09_ed6e2636.jpg"
 image = cv2.imread(image_path)
 
-# Splitting channels and computing histograms
 b, g, r, hist_b_before, hist_g_before, hist_r_before = split_and_compute_histograms(image)
 
-# Plotting original image and its histograms
 fig, axs = plt.subplots(3, 4, figsize=(15, 10))
 
-# Original Image
 axs[0, 0].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 axs[0, 0].set_title("Original Image")
 axs[0, 0].axis("off")
@@ -44,13 +40,10 @@ plot_histogram(axs[0, 1], hist_b_before, "blue", "Blue Channel Histogram (Before
 plot_histogram(axs[0, 2], hist_g_before, "green", "Green Channel Histogram (Before)")
 plot_histogram(axs[0, 3], hist_r_before, "red", "Red Channel Histogram (Before)")
 
-# Adjust contrast
 contrast_image = adjust_contrast(image)
 
-# Splitting channels and computing histograms after contrast adjustment
 b_contrast, g_contrast, r_contrast, hist_b_after_contrast, hist_g_after_contrast, hist_r_after_contrast = split_and_compute_histograms(contrast_image)
 
-# Plotting image after contrast adjustment and its histograms
 axs[1, 0].imshow(cv2.cvtColor(contrast_image, cv2.COLOR_BGR2RGB))
 axs[1, 0].set_title("Image after Increasing Contrast")
 axs[1, 0].axis("off")
@@ -59,13 +52,11 @@ plot_histogram(axs[1, 1], hist_b_after_contrast, "blue", "Blue Channel Histogram
 plot_histogram(axs[1, 2], hist_g_after_contrast, "green", "Green Channel Histogram (After)")
 plot_histogram(axs[1, 3], hist_r_after_contrast, "red", "Red Channel Histogram (After)")
 
-# Adjust brightness
 brightness_image = adjust_brightness(contrast_image)
 
-# Splitting channels and computing histograms after brightness adjustment
 b_brightness, g_brightness, r_brightness, hist_b_after_brightness, hist_g_after_brightness, hist_r_after_brightness = split_and_compute_histograms(brightness_image)
 
-# Plotting image after brightness adjustment and its histograms
+
 axs[2, 0].imshow(cv2.cvtColor(brightness_image, cv2.COLOR_BGR2RGB))
 axs[2, 0].set_title("Image after Decreasing Brightness")
 axs[2, 0].axis("off")
